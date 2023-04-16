56bbd3e4882ca94021c60e4331dc910e1cfc2e05
Jedsada Nuanyoi <jedsada@mdtth.com>
2023-04-16 16:21:54 +0700

    update : home card shown state

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+type Tab = 'my-cards' | 'public-cards'
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  currentTab: Tab = 'my-cards'
+
+  constructor() {}
+
+  updateTap(tab: Tab) {
+    this.currentTab = tab
+  }
 
 }
