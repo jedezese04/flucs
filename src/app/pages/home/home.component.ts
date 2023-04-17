3f8cb542c7aa6f02f57a61e41a61dff0204e559c
Jedsada Nuanyoi <jedezese04@gmail.com>
2023-04-17 21:17:27 +0700

    update : change observable pattern

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators'
+import { User } from 'src/app/models';
+import { AuthenticationService } from 'src/app/services';
 
 type Tab = 'my-cards' | 'public-cards'
 
@@ -8,12 +12,34 @@ type Tab = 'my-cards' | 'public-cards'
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
   currentTab: Tab = 'my-cards'
+  user!: User | null
+  private destroy$ = new Subject<void>()
 
   constructor(
-    private router: Router
+    private router: Router,
+    private auth: AuthenticationService
   ) {}
+
+  ngOnInit(): void {
+    this.currentTab = 'public-cards'
+    this.auth.user
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((value) => {
+        if(value) {
+          this.user = value
+        } else {
+          this.user = null
+          this.currentTab = 'public-cards'
+        }
+      })
+  }
+  
+  ngOnDestroy(): void {
+    this.destroy$.next()
+    this.destroy$.complete()
+  }
 
   updateTap(tab: Tab) {
     this.currentTab = tab
